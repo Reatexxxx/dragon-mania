6f1266ceac08adcb59e7d9e868a445ba0cf4036c
Reatexxxx <habsajalos@gmail.com>
2026-07-20 17:59:15 +0200

    main.js aktualisieren

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
-.
+const config = {
+    type: Phaser.AUTO,
+    parent: "game",
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 720,
+        height: 1280
+    },
+
+    backgroundColor: "#5cb8ff",
+
+    scene: {
+        create
+    }
+};
+
+new Phaser.Game(config);
+
+function create() {
+
+    this.add.text(
+        360,
+        200,
+        "Dragon-Mania",
+        {
+            fontSize: "54px",
+            color: "#ffffff",
+            fontStyle: "bold"
+        }
+    ).setOrigin(0.5);
+
+    this.add.text(
+        360,
+        320,
+        "Version 0.1",
+        {
+            fontSize: "30px",
+            color: "#ffffff"
+        }
+    ).setOrigin(0.5);
+
+}
